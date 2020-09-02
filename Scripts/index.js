cb8c145fdd4a34b92c412624b9eeb90d1e4c8579
MantaranSingh <meekmantaran@gmail.com>
2020-09-02 16:32:30 +0530

    burger animation added

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,4 +2,5 @@ let burger = document.querySelector('.burger')
 let sideBar = document.querySelector('.sideBar')
 burger.addEventListener('click', () => {
     sideBar.classList.toggle('navActive')
+    burger.classList.toggle('toggle')
 })
